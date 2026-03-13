ba26d2cf4a9f538479d12df4f4977e4f1134aa40
therealshashwattt <harshmishra9270@gmail.com>
2026-03-13 22:28:23 +0530

    Removed secrets and updated gitignore

--- config/firebase.php
+++
@@ -1,3 +0,0 @@
-<?php
-// 🔥 Put your Firebase Server Key here
-define("FIREBASE_SERVER_KEY", "PASTE_YOUR_FIREBASE_SERVER_KEY_HERE");
--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if(isset($_SESSION['student_id'])){
-    header("Location: student/dashboard.php");
-} else {
-    header("Location: auth/login.php");
-}
-exit;
